528374acf5ef986769d12a7163eea7788ba692d8
Juanma <jtoracu1211@g.educaand.es>
2025-11-26 19:37:18 +0100

    Añado suma

--- src/PruebaInt.java
+++ src/PruebaInt.java
@@ -3,5 +3,8 @@ public class PruebaInt {
         System.out.println("Hola mundo");
 
         int a = 1;
+        int b = 2;
+        int resultado = a + b;
+        System.out.println(resultado);
     }
 }
